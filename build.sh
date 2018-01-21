63b23366711928196208a0c3616613a571d57b0d
Luke Bonaccorsi <luke@lukeb.co.uk>
2018-01-21 03:01:21 +0000

    Move mjs build step to inside a container so it can be run on other platforms

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
 
-#docker login -u "$DOCKER_USER" -p "$DOCKER_PASS"
 git clone https://github.com/cesanta/mjs.git
-cd mjs/mjs
-export TAG=$(git describe --abbrev=0 --tags)
-git checkout $TAG
-make
-cd ../../
-docker build .
-# docker build -t lukebuk/mjs:latest -t lukebuk/mjs:$TAG .
-#docker push lukebuk/mjs
+cd mjs
+export MJS_TAG=$(git describe --abbrev=0 --tags)
+git checkout $MJS_TAG
+cd ../
+BUILD_IMAGE=$(docker build -q -f Dockerfile.build .)
+docker run -v $(pwd)/mjs:/mjs -w /mjs/mjs --privileged --rm -it $BUILD_IMAGE sh -c "dockerd > /dev/null 2>&1 & make"
+docker rmi $BUILD_IMAGE
+docker build -t lukebuk/mjs:latest -t lukebuk/mjs:$MJS_TAG .
+rm -rf mjs
