eee0516f5404eaca4d08513810d197ce15573bdb
Luke Bonaccorsi <luke.bonaccorsi@skybettingandgaming.com>
2018-02-20 13:39:58 +0000

    Add ability to build specific tag/branch

--- build.sh
+++ build.sh
@@ -2,11 +2,15 @@
 
 git clone https://github.com/cesanta/mjs.git
 cd mjs
-export MJS_TAG=$(git describe --abbrev=0 --tags)
+if [ -n "$1" ]; then
+    export MJS_TAG="$1"
+else
+    export MJS_TAG=$(git describe --abbrev=0 --tags)
+fi
 git checkout $MJS_TAG
 cd ../
 BUILD_IMAGE=$(docker build -q -f Dockerfile.build .)
 docker run -v $(pwd)/mjs:/mjs -w /mjs/mjs --privileged --rm -it $BUILD_IMAGE sh -c "dockerd > /dev/null 2>&1 & make"
 docker rmi $BUILD_IMAGE
-docker build -t lukebuk/mjs:latest -t lukebuk/mjs:$MJS_TAG .
+docker build -t lukebuk/mjs:$MJS_TAG .
 rm -rf mjs
